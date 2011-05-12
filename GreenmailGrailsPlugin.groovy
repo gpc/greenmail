6f7ecde8d735a6bc90c2c38dc8d7509aa30505a1
Luke Daley <ld@ldaley.com>
2011-05-12 13:31:11 +1000

    Bumped version to 1.3-SNAPSHOT

--- GreenmailGrailsPlugin.groovy
+++ GreenmailGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import com.icegreen.greenmail.util.ServerSetup
 
 class GreenmailGrailsPlugin {
     // the plugin version
-    def version = "1.2.2"
+    def version = "1.3-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1.1 > *"
     // the other plugins this plugin depends on
