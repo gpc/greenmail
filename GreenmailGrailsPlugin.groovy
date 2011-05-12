d86f6b4b39505e5922a7bc719efc6a756f663815
Luke Daley <ld@ldaley.com>
2011-05-12 13:32:57 +1000

    Cleaned up plugin descriptor

--- GreenmailGrailsPlugin.groovy
+++ GreenmailGrailsPlugin.groovy
@@ -3,29 +3,25 @@ import com.icegreen.greenmail.util.ServerSetupTest
 import com.icegreen.greenmail.util.ServerSetup
 
 class GreenmailGrailsPlugin {
-    // the plugin version
-    def version = "1.3-SNAPSHOT"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.1.1 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
 
-	def scopes = [excludes:'war']
+	def title = "Greenmail Plugin for Grails"
+	def description = "Provides a wrapper around GreenMail (http://www.icegreen.com/greenmail/) and provides a view that displays 'sent' messages"
+	def documentation = "http://grails.org/plugin/greenmail"
+	
+	def version = "1.3-SNAPSHOT"
+	def grailsVersion = "1.1.1 > *"
+	def dependsOn = [:]
 
-    def author = "Mike Hugo"
-    def authorEmail = "mike@piragua.com"
-    def title = "Greenmail Plugin for Grails"
-    def description = '''\\
-Provides a wrapper around GreenMail (http://www.icegreen.com/greenmail/) and provides a view that displays 'sent' messages
-'''
+	def author = "Mike Hugo"
+	def authorEmail = "mike@piragua.com"
 
-    def documentation = "http://grails.org/plugin/greenmail"
+	def pluginExcludes = [
+		"grails-app/views/error.gsp"
+	]
 
-    def doWithSpring = {
+	def scopes = [excludes: 'war']
+	
+	def doWithSpring = {
 		def smtpPort = application.config.greenmail.ports.smtp ?: ServerSetupTest.SMTP.port
 		def smtp = new ServerSetup(smtpPort, null, "smtp")
 	
@@ -33,30 +29,6 @@ Provides a wrapper around GreenMail (http://www.icegreen.com/greenmail/) and pro
 			it.initMethod = 'start'
 			it.destroyMethod = 'stop'
 		}
-        // TODO Implement runtime spring config (optional)
-    }
+	}
 
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def onChange = { event ->
-	    // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-	
 }
