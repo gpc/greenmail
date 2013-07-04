5b69953f19a9757588d622dac25115b2b65833b2
Sergey Ponomarev <stokito@gmail.com>
2013-07-05 02:31:58 +0300

    Revert accidentally change

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,4 +1,3 @@
 def smtpPort = com.icegreen.greenmail.util.ServerSetupTest.SMTP.port
 grails.mail.port = smtpPort
-greenmail.ports.smtp = smtpPortgrails.views.default.codec="none" // none, html, base64
-grails.views.gsp.encoding="UTF-8"
+greenmail.ports.smtp = smtpPort
